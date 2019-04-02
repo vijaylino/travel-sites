edb6a660393fe09ce87260ae2929a9af45328334
Lino <vijaylino@gmail.com>
2019-04-02 22:08:28 +0530

    BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     console.log("Hooray - we successfully installed gulp");
@@ -15,18 +16,31 @@ gulp.task('html',function(){
 });
 
 gulp.task('css',function(){
+    console.log("inside Preprocessor for CSS")
     gulp.src('./app/assets/styles/header.css')
     .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
     .pipe(gulp.dest('./app/temp/mystyle'));
-})
+});
 
 gulp.task('watch',function(){
 
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html',function(){
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css',function(){
-        gulp.start('css');
+        gulp.start('cssInject');
     });
+});
+
+gulp.task('cssInject',['css'],function(){
+    console.log("inside cssInject")
+    return gulp.src('/app/temp/mystyle/header.css')
+    .pipe(browserSync.stream());
 });
